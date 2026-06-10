51410aa8779becd4b84476461c5eed6c2651415c
WansFish <pereman0813@gmail.com>
2026-06-10 16:20:43 +0700

    Apply global AOS mirror animation config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useState, lazy, Suspense } from "react";
+import React, { useEffect, useState, lazy, Suspense } from "react";
 import { HelmetProvider } from "react-helmet-async";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
@@ -19,7 +21,33 @@ const ProjectDetails = lazy(() => import("./components/ProjectDetail"));
 const WelcomeScreen = lazy(() => import("./Pages/WelcomeScreen"));
 const NotFoundPage = lazy(() => import("./Pages/404"));
 
+const initScrollAnimations = () => {
+  AOS.init({
+    once: false,
+    mirror: true,
+    offset: 10,
+    duration: window.innerWidth < 640 ? 350 : 500,
+    delay: 0,
+    disable: false,
+  });
+  AOS.refreshHard();
+};
+
 const LandingPage = ({ showWelcome, setShowWelcome }) => {
+  useEffect(() => {
+    if (showWelcome) return;
+
+    const timers = [0, 500, 1500].map((delay) => setTimeout(initScrollAnimations, delay));
+    window.addEventListener("resize", initScrollAnimations);
+    window.addEventListener("orientationchange", initScrollAnimations);
+
+    return () => {
+      timers.forEach(clearTimeout);
+      window.removeEventListener("resize", initScrollAnimations);
+      window.removeEventListener("orientationchange", initScrollAnimations);
+    };
+  }, [showWelcome]);
+
   return (
     <>
       <AnimatePresence mode="wait">
@@ -33,7 +61,6 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
       {!showWelcome && (
         <>
           <Navbar />
-      
           <Home />
           <About />
           <Suspense fallback={<div className="h-20" />}>
@@ -60,11 +87,10 @@ function App() {
   const [showWelcome, setShowWelcome] = useState(true);
 
   return (
-    
     <HelmetProvider>
       <div className="pointer-events-none">
-  <AnimatedBackground />
-</div>
+        <AnimatedBackground />
+      </div>
       <BrowserRouter>
         <Routes>
           {/* PUBLIC */}
